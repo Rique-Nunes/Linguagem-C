a10a98a1a8d71b70819091d65af69e203e75192e
Henrique <henriquenunesrj09@gmail.com>
2024-10-22 17:45:45 -0300

    atualizacao

---
+++ aprendizado/lista8/quest3.c
@@ -0,0 +1,15 @@
+/*Desenvolver uma função que calcule o n-ésimo
+termo da sequência de Fibonacci.*/
+#include <stdio.h>
+void fibo(int numero)
+{
+    if (numero < 2)
+    {
+        return 1;
+    }
+    int a = 1, b = 2, c = 3;
+    printf("%d", a);
+    for (int i = 3; i <= numero; i++){
+        printf("%d" a)
+    }
+}
