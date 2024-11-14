45326185be5a76704d9dc42ff7cade5bd5bdc86b
Henrique <henriquenunesrj09@gmail.com>
2024-11-14 20:42:07 -0300

    finalmente o atv8-exericicio difícil consertado

--- aprendizado/exercicio_dificil/atv8.c
+++ aprendizado/exercicio_dificil/atv8.c
@@ -37,39 +37,30 @@ void main()
 int vetorial(int lista[], int numero, int tamanho, int sub_vetor[], int *sub_indice)
 {
     int indice = 0;
-    int qtd_zeros = numero, contador_zero = 0;
+    int contador_zero = 0;
 
     // rodando pelo vetor inteiro
     for (int i = 0; i < tamanho; i++)
     {
-        // quero procurar o zero correspondente a posição que o usuário escolheu
-        // se ele digitar 1, vai mostrar o subconjunto anterior ao primeiro zero e assim por diante
+        /*basicamente só vai adicionar ao subvetor[] quando o contador_zero for
+        menor ou igual ao número que a pessoa digitou -1, por exemplo se foi 1,
+        o contador de zeros vai ser 0 até ele encontrar o primeiro zero e se tornar 1 e parar a condição*/
         if (lista[i] == 0)
         {
-            contador_zero += 1;
+            contador_zero++;
         }
 
-        if (contador_zero == qtd_zeros)
+        if (contador_zero == numero)
         {
-            // o I está na posição do zero, (3 por exemplo), mais queremos o subconjunto anterior a ele
-            // então diminui 1 para iniciar a contagem, temos duas condições um para parar no zero e outra para
-            // limitar o tamanho do sub_vetor, o J vai voltando posições até ele representar a posição do zero
-            for (int j = i - 1; lista[j] == 0 || indice < 3; j--)
-            {
-                // isso foi para caso o subconjunto for menor e não incluir o zero
-                if (lista[j] != 0)
-                {
-                    sub_vetor[indice] = lista[j];
-                    indice++;
-                }
-                // sem essa parte se voce escolher o 3° subconjunto (ou outros) iria incluir numeros a mais
-                if (lista[j] == 0)
-                {
-                    break;
-                }
-            }
-            // isso é para não ler o resto da varredura da lista
             break;
+        }
+
+        if (contador_zero >= numero - 1 && contador_zero < numero)
+        {
+            if (lista[i] != 0)
+            {
+                sub_vetor[indice++] = lista[i];
+            }
         }
     }
     *sub_indice = indice;
