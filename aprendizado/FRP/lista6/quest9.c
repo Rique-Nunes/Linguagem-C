21ab183044c1ba35b0a59e9120131d5e28b662e2
Henrique <91799317+Rique-Nunes@users.noreply.github.com>
2025-04-09 10:58:25 -0300

    Tentando atualizar

--- aprendizado/FRP/lista6/quest9.c
+++ aprendizado/FRP/lista6/quest9.c
@@ -1,34 +1,37 @@
 /*Desenvolver uma função recursiva que, dado um vetor V com quant números inteiros,
 determine se seus elementos estão dispostos
-de maneira a representar uma progressãoaritmética.
+de maneira a representar uma progressão aritmética.
 */
 #include <stdio.h>
 #include <string.h>
 
-void aritmetica(int quant, int vet[quant], int *testador);
+int aritmetica(int quant, int vet[quant], int *testador, int resultado);
 
 void main()
 {
-    int testador;
-    char string[] = "avenida"; // String de teste
-    int tamanho = strlen(string);
-    inverte_string(string, tamanho - 1);
+    int testador, resultado;
+    int string[5] = {2,4,6,8,10}; // String de teste
+    int tamanho = 5;
+    testador = string[tamanho - 1] - string[tamanho - 2];
+    resultado = aritmetica(tamanho - 1, string, &testador, 1);
 }
 
-void aritmetica(int quant, int vet[quant], int *testador)
+int aritmetica(int quant, int vet[quant], int *testador, int resultado)
 {
     if (quant < 1)
     {
-        return 0;
+        return 1;
     }
-    aritmetica(quant - 1, vet, *testador);
+    resultado = aritmetica(quant, vet, *testador, resultado);
+    if(resultado == 1){
     if (vet[quant] - vet[quant - 1] == *testador)
     {
-        *testador = 1;
+        return 1;
     }
     else
     {
-        
+        return 0;
     }
+}
     
 }
