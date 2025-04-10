23928e156321dd290494f4ceea7475e864a6488d
Henrique <91799317+Rique-Nunes@users.noreply.github.com>
2025-04-10 16:59:01 -0300

    Questão 9 feita com sucesso!

--- aprendizado/FRP/lista6/quest9.c
+++ aprendizado/FRP/lista6/quest9.c
@@ -10,10 +10,11 @@ int aritmetica(int quant, int vet[quant], int *testador, int resultado);
 void main()
 {
     int testador, resultado;
-    int string[5] = {2,4,6,8,10}; // String de teste
-    int tamanho = 5;
+    int string[5] = {2, 3, 6, 8, 10}; // String de teste
+    int tamanho = 5; //o tamanho real é 6 ai que bugou tudo
     testador = string[tamanho - 1] - string[tamanho - 2];
     resultado = aritmetica(tamanho - 1, string, &testador, 1);
+    printf("%d", resultado);
 }
 
 int aritmetica(int quant, int vet[quant], int *testador, int resultado)
@@ -22,16 +23,20 @@ int aritmetica(int quant, int vet[quant], int *testador, int resultado)
     {
         return 1;
     }
-    resultado = aritmetica(quant, vet, *testador, resultado);
-    if(resultado == 1){
-    if (vet[quant] - vet[quant - 1] == *testador)
+    resultado = aritmetica(quant - 1, vet, testador, resultado); //testador aqui é variavel e não ponteiro
+    if (resultado == 1)
     {
-        return 1;
+        if (vet[quant] - vet[quant - 1] == *testador)
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
     }
-    else
+    else //else para quando o primeiro return de 0 acontecer nem precisa testar os outros
     {
         return 0;
     }
-}
-    
 }
