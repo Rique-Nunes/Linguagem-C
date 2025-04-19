abf659cc201acbcb0ab202b9ce42cb7a5f3faf78
Henrique <henriquenunesrj09@gmail.com>
2025-04-19 00:11:22 -0300

    questão 10 melhorada

--- aprendizado/FRP/lista6/quest10.c
+++ aprendizado/FRP/lista6/quest10.c
@@ -12,10 +12,11 @@ void main()
 {
     char string[] = "testando";
     int tamanho = strlen(string), contador = 0, teste;
-    char caractere_rev = "t";
-
-    teste = removedor_str(tamanho - 1, string, caractere_rev, &contador);
-    printf("%d : %d", teste, contador);
+    char caractere_rev = 't';
+    printf("%s", string);
+    removedor_str(tamanho - 1, string, caractere_rev, &contador);
+    printf("\nremocoes: %d", contador);
+    printf("\n%s", string);
 }
 
 int removedor_str(int quant, char str[], char caractere, int *cont)
@@ -27,10 +28,11 @@ int removedor_str(int quant, char str[], char caractere, int *cont)
     *cont += removedor_str(quant - 1, str, caractere, cont);
     if (str[quant] == caractere)
     {
-        for (int i = quant + 1; i < strlen(str); i++)
+        for (int i = quant; i < strlen(str); i++)
         {
-            str[quant] = str[i];
             str[i] = str[i + 1];
+            //banana
+            //b - a, a - n, n - a, a - n, n - a, a - \0
         }
         return 1;
     }
