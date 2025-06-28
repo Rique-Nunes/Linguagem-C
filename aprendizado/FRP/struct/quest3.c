1bef3646635d809aa66e0d0a89445bc4e8ccc32e
Henrique <henriquenunesrj09@gmail.com>
2025-06-27 21:59:42 -0300

    A

--- aprendizado/FRP/struct/quest3.c
+++ aprendizado/FRP/struct/quest3.c
@@ -7,28 +7,37 @@ typedef struct
     char genero;
 } talunos;
 
+void ordenador(talunos alunos[], char genero, int tamanho);
 void main()
 {
-    talunos lista[3] = {{1,"m"}, {2,"m",}, {3, "f"}};
-    
-    
+    talunos lista[3] = {{1, 'm'}, {
+                                      2,
+                                      'm',
+                                  },
+                        {3, 'f'}};
+
+    ordenador(lista, 'f', 3);
+    for (int i = 0; i < 3; i++)
+    {
+        printf("%d", lista[i].matricula);
+    }
 }
 
 void ordenador(talunos alunos[], char genero, int tamanho)
 {
     int i;
-    char trocar;
+    talunos trocar;
     for (i = 0; i < tamanho; i++)
     {
         if (alunos[i].genero == genero)
         {
-            for (int j = i + 1; j < tamanho; j++)
+            for (int j = 0; j < tamanho; j++)
             {
                 if (alunos[j].genero != genero)
                 {
-                    trocar = alunos[i].genero;
-                    alunos[i].genero = alunos[j].genero;
-                    alunos[j].genero = trocar;
+                    trocar = alunos[i];
+                    alunos[i] = alunos[j];
+                    alunos[j] = trocar;
                     break;
                 }
             }
