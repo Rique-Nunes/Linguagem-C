cc72200d816c75f3a134adc0907b799e0cb5bb74
Henrique <henriquenunesrj09@gmail.com>
2025-08-21 18:14:14 -0300

    atualização dos comentários

--- aprendizado/ESD/matriz.c
+++ aprendizado/ESD/matriz.c
@@ -21,27 +21,65 @@ void main()
 {
     // int matriz[3][4];
     int lin = 3, col = 4;
-    // 1° método uma alocação linear onde só tem um ponteiro bem grande
-    // int *matriz = malloc(lin * col * sizeof(int));
 
+
+    // 1° método uma alocação linear onde só tem um ponteiro bem grande
+
+    // int *matriz = malloc(lin * col * sizeof(int));
+    // preencheraleatorio_v1(lin, col, matriz);
+    // free(matriz); //para v1
+
+
+    
     // 2° método uma alocação de em um vetor de vetores
-    //int **matriz = malloc(lin * sizeof(int *));
-    //for (int k = 0; k < lin; k++)
-    /*{
+    /*
+    //criar
+    
+    int **matriz = malloc(lin * sizeof(int *));
+    for (int k = 0; k < lin; k++)
+    {
         matriz[k] = malloc(col * sizeof(int));
     }
 
+    //preencher
     preencheraleatorio_v2(3, 4, matriz);
-    */
-   //método 3 uma alocação de vetores em matriz[0]
 
-   int **matriz = malloc( lin * sizeof(int*));
-   matriz[0] = malloc(lin * col *sizeof(int));
-   
-   for(int i=1; i<lin; i++){
-    matriz[i]=matriz[0]+i*col;
-   }
-    //o v2 serve para o v3 pois ambos são acessados por matriz[i][j], sendo a matriz **
+        for (int i = 0; i < 3; i++)
+    {
+        printf("\n");
+        for (int j = 0; j < 4; j++)
+        {
+            printf("%d,", matriz[(i * col) + j]);
+        }
+    }
+    printf("\n");
+
+
+    printf("\n");
+    mostrarqtdcolunas_v2(lin, col, matriz);
+    printf("\n");
+    mostrarqtdlinhas_v2(lin, col, matriz);
+    printf("\n");
+    
+    // Liberar a memória alocada
+    for (int k = 0; k < lin; k++)
+    {
+        free(matriz[k]);
+    }
+
+    free(matriz);
+
+    */
+    // método 3 uma alocação de vetores em matriz[0]
+
+    int **matriz = malloc(lin * sizeof(int *));
+    matriz[0] = malloc(lin * col * sizeof(int));
+
+    for (int i = 1; i < lin; i++)
+    {
+        matriz[i] = matriz[0] + i * col;
+    }
+    // o v2 serve para o v3 pois ambos são acessados por matriz[i][j], sendo a matriz **
     preencheraleatorio_v2(3, 4, matriz);
     for (int i = 0; i < 3; i++)
     {
@@ -53,20 +91,9 @@ void main()
     }
     free(matriz[0]);
     free(matriz);
-    //printf("\n");
-    //mostrarqtdcolunas_v2(lin, col, matriz);
-    //printf("\n");
-    //mostrarqtdlinhas_v2(lin, col, matriz);
-    //printf("\n");
 
-    /*
-    for (int k = 0; k < lin; k++)
-    {
-        free(matriz[k]);
-    }
-    */
-    //free(matriz);
-    // preencheraleatorio_v1(lin, col, matriz);
+
+
     /*
     for (int i = 0; i < 3; i++)
     {
@@ -78,7 +105,6 @@ void main()
     }
     printf("\n");
     */
-    // free(matriz); //para v1
 
     //  mostrarqtdlinhas(3, 4, matriz);
     //  printf("\n");
