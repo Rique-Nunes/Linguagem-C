0b2ad95582e0aad6572d55fd1d257b1e6dba10f3
Henrique <henriquenunesrj09@gmail.com>
2025-08-28 16:45:36 -0300

    parte final do trabalho

--- aprendizado/ESD/exercicio_atleta.c
+++ aprendizado/ESD/exercicio_atleta.c
@@ -9,20 +9,20 @@ struct atleta
     int tempo;
 };
 typedef struct atleta vAtletas;
-vAtletas *cadastrar_atletas(int n);
+
+vAtletas *cadastrar_atletas_inicial(int n);
+void preencher_novos_atletas(vAtletas *vetor, int inicio, int fim);
 
 int main()
 {
-    int opcao = 0, n = 0, trocou = 1;
+    int opcao = 0, n_total = 0, trocou = 1;
     vAtletas *vetor = NULL;
-    vAtletas **indices = NULL;
     vAtletas **indices_nome = NULL;
     vAtletas **indices_ranking = NULL;
 
     do
     {
-        printf("\n===== MENU DE ATLETAS =====\n");
-        printf("1. Cadastrar novos atletas\n");
+        printf("\n1. Cadastrar/Adicionar atletas\n");
         printf("2. Ver ordem de cadastro\n");
         printf("3. Ver ranking por tempo\n");
         printf("4. Ver ordem alfabetica\n");
@@ -33,39 +33,74 @@ int main()
         switch (opcao)
         {
         case 1:
-            printf("Quantos atletas ira cadastrar? ");
-            scanf("%d", &n);
+        {
+            int n_adicionar;
+            printf("Quantos atletas deseja adicionado? ");
+            scanf("%d", &n_adicionar);
 
-            if (n > 0)
+            if (n_adicionar <= 0)
+                break;
+
+            int novo_tamanho_total = n_total + n_adicionar;
+
+            if (n_total == 0)
             {
-                vetor = cadastrar_atletas(n);
+                vetor = cadastrar_atletas_inicial(n_adicionar);
                 if (vetor == NULL)
-                    return 0;
-
-                indices_nome = (vAtletas **)malloc(n * sizeof(vAtletas *));
-                indices_ranking = (vAtletas **)malloc(n * sizeof(vAtletas *));
-
-                for (int i = 0; i < n; i++)
-                {
-                    indices_nome[i] = &vetor[i];
-                    indices_ranking[i] = &vetor[i];
-                }
+                    return 1;
             }
+            else
+            {
+                vAtletas *temp_vetor = realloc(vetor, novo_tamanho_total * sizeof(vAtletas));
+                if (temp_vetor == NULL)
+                {
+                    printf("Erro ao realocar o vetor de atletas!\n");
+                    break;
+                }
+                vetor = temp_vetor;
+
+                preencher_novos_atletas(vetor, n_total, novo_tamanho_total);
+            }
+
+            vAtletas **temp_nome = realloc(indices_nome, novo_tamanho_total * sizeof(vAtletas *));
+            vAtletas **temp_ranking = realloc(indices_ranking, novo_tamanho_total * sizeof(vAtletas *));
+
+            if (temp_nome == NULL || temp_ranking == NULL)
+            {
+                printf("Erro ao realocar os vetores de indices!\n");
+                break;
+            }
+            indices_nome = temp_nome;
+            indices_ranking = temp_ranking;
+
+            n_total = novo_tamanho_total;
+
+            for (int i = 0; i < n_total; i++)
+            {
+                indices_nome[i] = &vetor[i];
+                indices_ranking[i] = &vetor[i];
+            }
+            printf("%d atletas adicionados com sucesso.\n", n_adicionar);
             break;
+        }
 
         case 2:
+            if (n_total == 0)
+            {
+                printf("\nNenhum atleta cadastrado.\n");
+                break;
+            }
             printf("\n--- Ordem de Cadastro ---\n");
-            for (int i = 0; i < n; i++)
+            for (int i = 0; i < n_total; i++)
             {
                 printf("Nome: %s, Matricula: %d, tempo: %d\n", vetor[i].nome, vetor[i].matricula, vetor[i].tempo);
             }
             break;
-
         case 3:
-            for (int i = 0; i < n - 1; i++)
+            for (int i = 0; i < n_total - 1; i++)
             {
                 int min = i;
-                for (int j = i + 1; j < n; j++)
+                for (int j = i + 1; j < n_total; j++)
                 {
                     if (indices_ranking[j]->tempo < indices_ranking[min]->tempo)
                     {
@@ -80,7 +115,7 @@ int main()
                 }
             }
             printf("\n--- Ordem por Ranking (Tempo) ---\n");
-            for (int i = 0; i < n; i++)
+            for (int i = 0; i < n_total; i++)
             {
                 printf("Nome: %s, Matricula: %d, tempo: %d\n", indices_ranking[i]->nome, indices_ranking[i]->matricula, indices_ranking[i]->tempo);
             }
@@ -90,7 +125,7 @@ int main()
             while (trocou == 1)
             {
                 trocou = 0;
-                for (int i = 0; i < n - 1; i++)
+                for (int i = 0; i < n_total - 1; i++)
                 {
                     if (strcmp(indices_nome[i]->nome, indices_nome[i + 1]->nome) > 0)
                     {
@@ -102,7 +137,7 @@ int main()
                 }
             }
             printf("\n--- Ordem Alfabetica por Nome ---\n");
-            for (int i = 0; i < n; i++)
+            for (int i = 0; i < n_total; i++)
             {
                 printf("Nome: %s, Matricula: %d, tempo: %d\n", indices_nome[i]->nome, indices_nome[i]->matricula, indices_nome[i]->tempo);
             }
@@ -111,7 +146,6 @@ int main()
         case 5:
             printf("Saindo...\n");
             break;
-
         default:
             printf("\nOpcao invalida\n");
             break;
@@ -119,26 +153,26 @@ int main()
 
     } while (opcao != 5);
 
-    for (int i = 0; i < n; i++)
+    if (vetor != NULL)
     {
-        free(vetor[i].nome);
+        for (int i = 0; i < n_total; i++)
+        {
+            free(vetor[i].nome);
+        }
+        free(vetor);
+        free(indices_ranking);
+        free(indices_nome);
     }
-    free(vetor);
-    free(indices_ranking);
-    free(indices_nome);
 
     printf("Programa finalizado.\n");
-    return 1;
+    return 0;
 }
 
-vAtletas *cadastrar_atletas(int n)
+vAtletas *cadastrar_atletas_inicial(int n)
 {
     vAtletas *vetor = (vAtletas *)malloc(n * sizeof(vAtletas));
     if (!vetor)
-    {
-        printf("Erro de alocação no vetor principal!\n");
         return NULL;
-    }
 
     for (int i = 0; i < n; i++)
     {
@@ -146,22 +180,29 @@ vAtletas *cadastrar_atletas(int n)
         printf("\n--- Atleta %d ---\n", i + 1);
         printf("Digite o nome: ");
         scanf("%s", nome_buffer);
-
         vetor[i].nome = (char *)malloc((strlen(nome_buffer) + 1) * sizeof(char));
-        if (!vetor[i].nome)
-        {
-            for (int j = 0; j < i; j++)
-                free(vetor[j].nome);
-            free(vetor);
-            return NULL;
-        }
         strcpy(vetor[i].nome, nome_buffer);
-
         printf("Digite a matricula: ");
         scanf("%d", &vetor[i].matricula);
-
         printf("Digite o tempo: ");
         scanf("%d", &vetor[i].tempo);
     }
     return vetor;
+}
+
+void preencher_novos_atletas(vAtletas *vetor, int inicio, int fim)
+{
+    for (int i = inicio; i < fim; i++)
+    {
+        char nome_buffer[100];
+        printf("\n--- Atleta %d ---\n", i + 1);
+        printf("Digite o nome: ");
+        scanf("%s", nome_buffer);
+        vetor[i].nome = (char *)malloc((strlen(nome_buffer) + 1) * sizeof(char));
+        strcpy(vetor[i].nome, nome_buffer);
+        printf("Digite a matricula: ");
+        scanf("%d", &vetor[i].matricula);
+        printf("Digite o tempo: ");
+        scanf("%d", &vetor[i].tempo);
+    }
 }
